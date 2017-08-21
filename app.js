a1402b2d3067cd3a2df20fcfb8b487c04d51431b
PUGE <100284685@qq.com>
2017-08-21 11:28:54 +0800

    1.增加屏幕重命名功能 2.删除屏幕功能

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.__esModule = true;
 const express    = require("express"),
+      chalk      = require('chalk'),
       fs         = require("fs"),
       low        = require('lowdb'),
       app        = express()
@@ -63,10 +64,28 @@ const db = low('.\\database\\config.json')
         body += chunk; //读取参数流转化为字符串
     });
     req.on('end', function () {
-      console.log('收到写屏幕配置项请求!')
-      db.defaults({ screen: [] }).write()
-      db.get('screen').push(JSON.parse(body)).write()
-      res.send({err: null});
+      // 产生随机字符串方法
+      function randomWord(num) {
+        const arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        let   str = ""
+        // 随机产生
+        for(let i = 0; i < num; i++){
+          const pos = Math.round(Math.random() * (arr.length-1));
+          str += arr[pos];
+        }
+        return str;
+      }
+      const id = randomWord(8)
+      const data = {
+        id: id,
+        name: '新建屏幕',
+        data: JSON.parse(body)
+      }
+      console.log(chalk.gray('[getScreenConfig]设置数据:'))
+      console.log(data)
+      db.defaults({ screen: {} }).write()
+      db.set(`screen.${id}`, data).write()
+      res.send({err: null, id: id});
     })
   });
 
@@ -84,8 +103,8 @@ const db = low('.\\database\\config.json')
         err: null,
         data: screenConfig[key - 1]
       }
-      console.log(`[getScreenConfig]返回数据:`)
-      console.log(data)
+      console.log(chalk.gray('[getScreenConfig]返回数据:'))
+      console.log(data.data)
       res.send(JSON.stringify(data));
     })
   });
@@ -93,12 +112,51 @@ const db = low('.\\database\\config.json')
   // 处理获取屏幕配置请求
   app.get('/getScreenNumber', function (req, res) {
     res.set('Access-Control-Allow-Origin','*');
-    const length = db.get('screen').value().length
-    res.send({err: null, length: length});
+    const value = db.get('screen').value()
+    let brief = []
+    for (let item in value) {
+      brief.push({
+        id: value[item].id,
+        name: value[item].name
+      })
+    }
+    res.send({err: null, brief: brief});
+  });
+
+  // 重命名屏幕
+  app.post('/screenRename', function (req, res) {
+    let body = '';
+    res.set('Access-Control-Allow-Origin','*');
+    req.on('data', function (chunk) {
+        body += chunk; //读取参数流转化为字符串
+    });
+    req.on('end', function () {
+      const data = JSON.parse(body)
+      console.log(chalk.gray('[screenRename]重命名屏幕:'))
+      console.log(data)
+      db.set(`screen.${data.id}.name`, data.newName).write()
+      res.send({err: null, name: data.newName});
+    })
+  });
+
+  // 删除屏幕
+  app.post('/delateScreen', function (req, res) {
+    let body = '';
+    res.set('Access-Control-Allow-Origin','*');
+    req.on('data', function (chunk) {
+        body += chunk; //读取参数流转化为字符串
+    });
+    req.on('end', function () {
+      const id = JSON.parse(body).id
+      console.log(chalk.gray('[delateScreen]删除屏幕:'))
+      console.log(id)
+      db.unset(`screen.${id}`).write()
+      res.send({err: null});
+    })
   });
 }
 
 // 服务器实例
 const server = app.listen(9999, function() {
-  console.log('服务正在监听9999端口!');
+  console.log(chalk.gray('服务正在监听9999端口!'));
 });
